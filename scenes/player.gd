e0d0b33f7e2a19080d332df4f647d26e4da96725
Felipe Boza <fboza@sps.ed.cr>
2026-05-25 19:01:32 -0600

    Movimiento básico de Desivinte

---
+++ scenes/player.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+const SPEED = 300.0
+const SPEED_FOCUSED = 150.0
+var is_focused = false
+func _physics_process(delta: float) -> void:
+	is_focused = Input.is_action_pressed("ui_select")
+	var speed = SPEED_FOCUSED if is_focused else SPEED
+	var direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	velocity = direction*speed
+	move_and_slide()
+	
