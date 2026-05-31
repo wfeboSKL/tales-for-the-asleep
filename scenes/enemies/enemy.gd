cb5ef3ac175dcb65ac10a1f9bcd6b9d1f9726acc
Felipe Boza <fboza@sps.ed.cr>
2026-05-30 22:27:24 -0600

    IA del enemigo y feedback visual de daño

--- scenes/enemies/enemy.gd
+++ scenes/enemies/enemy.gd
@@ -7,20 +7,28 @@ const SPEED = 150.0
 var health = 3
 #Se establece el cooldown de la bala inicial
 var shoot_cooldown = 0.0
+#Referencia al jugador, inicia vacía hasta que la escena esté lista
+var player = null
 #Se "precarga" la escena de la bala enemiga
 const ENEMY_BULLET = preload("res://scenes/bullets/enemy_bullet.tscn")
+#Se establece el cooldown del parpadeo del sprite
+var damageindicator_cooldown = 0.0
 
 #--MOVIMIENTO DEL ENEMIGO--
 #Función que calcula los procesos físicos cada frame
 func _physics_process(delta: float) -> void:
 	#Velocidad del enemigo será hacia la izquierda
 	velocity.x = -SPEED
+	if player:
+		velocity.y = move_toward(velocity.y, (player.position.y - position.y), SPEED)
 	#Se hace el cálculo final del movimiento
 	move_and_slide()
 	
 	#--DISPARO DEL ENEMIGO--
 	#Se le resta al cooldown delta (1/fps del juego)
 	shoot_cooldown -= delta
+	#Se le resta al cooldown delta (1/fps del juego)
+	damageindicator_cooldown -= delta
 	#Si el cooldown es menor o igual a 0
 	if shoot_cooldown <= 0:
 		#La variable bala crea (o instancia) la escena ENEMY_BULLET
@@ -32,6 +40,12 @@ func _physics_process(delta: float) -> void:
 		#Se reestablece el cooldown a 0.9
 		shoot_cooldown = 0.9
 	
+	#Si el cooldown es mayor a 0, parpadea cada 0.05 segundos
+	if damageindicator_cooldown > 0:
+		$Sprite2D.visible = fmod(damageindicator_cooldown, 0.2) > 0.05
+	else:
+		#De lo contrario, será siempre visible
+		$Sprite2D.visible = true
 	#Si la posición del enemigo es -50 fuera de la pantalla, eliminarlo por completo
 	if position.x< -50:
 		queue_free()
@@ -40,7 +54,11 @@ func _physics_process(delta: float) -> void:
 func take_damage(amount):
 	#A la vida, se le restará amount
 	health -= amount
+	#Se reinicia el cooldown
+	damageindicator_cooldown = 1.5
 	#Si la vida es menor a cero, eliminar por completo al enemigo
 	if health <= 0:
 		queue_free()
-	
+#Obtener el primer nodo del grupo "player" para seguirlo
+func _ready():
+	player = get_tree().get_first_node_in_group("Player")
