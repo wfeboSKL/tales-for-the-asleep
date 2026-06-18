6eb0f83e7e1870c13bbcfdd31e2c1b26b7bfb37c
Felipe Boza <fboza@sps.ed.cr>
2026-06-17 19:33:43 -0600

    Agregar sistema de XP básico y niveles a GameData | Trabajado en PC_FEBO

--- game_data.gd
+++ game_data.gd
@@ -1,14 +1,29 @@
-#-- VARIABLES DE LA INFORMACIÓN DEL JUEGO EN GENERAL --
 extends Node
-#Puntaje
+
+# -- VARIABLES DE LA INFORMACIÓN DEL JUEGO EN GENERAL --
+
+# Puntaje
 var score = 0
-#Variables que registran kills y horda actual
+# Variables que registran kills y horda actual
 var enemies_killed = 0
 var current_wave = 1
 
-#-- FUNCIÓN PARA RESETEAR LA INFORMACIÓN UNA VEZ QUE SE OCUPE --
-#Cuando el jugador muera básicamente
+# -- VARIABLES DE XP Y NIVEL --
+var player_xp = 0
+var player_level = 1
+var xp_to_next_level = 50
+
+# -- FUNCIÓN PARA SUBIR DE NIVEL --
+func level_up():
+	player_level += 1
+	player_xp = 0
+	xp_to_next_level = 50 * player_level
+
+# -- FUNCIÓN PARA RESETEAR LA INFORMACIÓN AL MORIR --
 func reset():
 	score = 0
 	enemies_killed = 0
 	current_wave = 1
+	player_xp = 0
+	player_level = 1
+	xp_to_next_level = 50
